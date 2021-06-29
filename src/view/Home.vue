ffc8eead4be5ee193ef1078de430d3a2f79d779e
Leslie <leslie.lianggangwei@foxmail.com>
2021-06-29 10:42:33 +0800

    add Home.vue CSS

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -3,14 +3,16 @@
   <div class="banner">
     <h1>WheelHub-Plus</h1>
     <h2>WheelHub 加强版，基于 Vue 3 开发的前端 UI 框架</h2>
-    <a href="">GitHub</a>
-    <span>  ||  </span>
-    <router-link to="/doc">开始</router-link>
+    <p class="actions">
+      <a href="https://github.com/Leslie-LiangGangwei/WheelHub-Plus">GitHub</a>
+      <router-link to="/doc">开始</router-link>
+    </p>
+
   </div>
 </template>
 
 <script>
-import Nav from '../components/Nav.vue'
+import Nav from "../components/Nav.vue"
 
 export default {
   name: "Home",
@@ -19,4 +21,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.banner {
+  padding: 100px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background: lightgreen;
+  > .actions {
+    padding: 8px 0;
+    a {
+      margin: 0 8px;
+      background: #fff;
+      display: inline-block;
+      $h: 28px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 8px;
+    }
+  }
+}
 </style>
