235b00642f22e78fe593cf7c0e4885c4124c99cb
Leslie <leslie.lianggangwei@foxmail.com>
2021-07-04 17:11:45 +0800

    Doc CSS

--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -1,26 +1,29 @@
+<
 <template>
-  <Nav></Nav>
-  <div class="doc-content">
-    <aside :class="{'mobile-show-aside': menuVisible}" ref="aside">
-      <h2>组件列表</h2>
-      <ol>
-        <li>
-          <router-link to="/doc/switch">Switch 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/button">Button 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/dialog">Dialog 组件</router-link>
-        </li>
-        <li>
-          <router-link to="/doc/tabs">Tabs 组件</router-link>
-        </li>
-      </ol>
-    </aside>
-    <main>
-      <router-view></router-view>
-    </main>
+  <div class="layout">
+    <Nav class="nav"></Nav>
+    <div class="doc-content">
+      <aside :class="{'mobile-show-aside': menuVisible}" ref="aside">
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
+      <main>
+        <router-view></router-view>
+      </main>
+    </div>
   </div>
 </template>
 
@@ -49,19 +52,40 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  .doc-content {
+    display: flex;
+    flex-grow: 1;
+    padding-top: 64px;
+    padding-left: 158px;
+    @media (max-width: 576px) {
+      padding-left: 0;
+    }
+    main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
+    }
+  }
+}
+
 aside {
   display: inline-block;
-  background: lightblue;
-  width: 150px;
-  padding: 16px;
   position: fixed;
   top: 64px;
   left: 0;
-  > h2 {
+  height: 100%;
+  width: 150px;
+  padding: 16px;
+  background: lightblue;
+  h2 {
     margin-bottom: 4px;
   }
-  > ol {
-    > li {
+  ol {
+    li {
       padding: 4px 0;
     }
   }
