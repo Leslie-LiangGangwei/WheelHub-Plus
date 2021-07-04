ec9405910e8991694e69065ec330be71da5be67e
Leslie <leslie.lianggangwei@foxmail.com>
2021-07-04 19:43:29 +0800

    Home 添加 aside

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,5 +1,22 @@
 <template>
   <Nav></Nav>
+  <aside :class="{'mobile-show-aside': menuVisible}" ref="aside">
+    <h2>组件列表</h2>
+    <ol>
+      <li>
+        <router-link to="/doc/switch">Switch 组件</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/button">Button 组件</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/dialog">Dialog 组件</router-link>
+      </li>
+      <li>
+        <router-link to="/doc/tabs">Tabs 组件</router-link>
+      </li>
+    </ol>
+  </aside>
   <div class="banner">
     <h1>WheelHub-Plus</h1>
     <h2>WheelHub 加强版，基于 Vue 3 开发的前端 UI 框架</h2>
@@ -11,12 +28,27 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Nav from "../components/Nav.vue"
+import {inject, nextTick, ref} from "vue";
 
 export default {
   name: "Home",
-  components: {Nav}
+  components: {Nav},
+  setup: function () {
+    const menuVisible = inject<ref<boolean>>('menuVisible')
+    const aside = ref(null);
+    const eventMenu = (event) => {
+      if (event.target == aside.value || aside.value.contains(event.target)) {
+        return
+      }
+      menuVisible.value = false
+    }
+    nextTick(() => {
+      document.addEventListener('click', eventMenu)
+    })
+    return {menuVisible, aside}
+  }
 }
 </script>
 
@@ -40,6 +72,31 @@ export default {
       border-radius: $h/2;
       padding: 0 8px;
     }
+  }
+}
+
+aside {
+  display: none;
+  position: fixed;
+  top: 64px;
+  left: 0;
+  height: 100%;
+  width: 150px;
+  padding: 16px;
+  background: lightblue;
+  h2 {
+    margin-bottom: 4px;
+  }
+  ol {
+    li {
+      padding: 4px 0;
+    }
+  }
+}
+
+@media (max-width: 576px) {
+  aside.mobile-show-aside {
+    display: inline-block;
   }
 }
 </style>
