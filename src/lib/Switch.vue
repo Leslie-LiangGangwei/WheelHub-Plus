87c46aeae01bf56ecb9bd98cb6c5f2f4a0d67212
Leslie <leslie.lianggangwei@foxmail.com>
2021-07-05 18:10:01 +0800

    Switch CSS

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>switch</div>
+  <button><span></span></button>
 </template>
 
 <script lang="ts">
@@ -9,5 +9,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$h: 22px;
+$h2: $h - 4px;
+button {
+  position: relative;
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: blue;
+  border-radius: $h/2;
+  outline: none;
+}
 
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+}
 </style>
