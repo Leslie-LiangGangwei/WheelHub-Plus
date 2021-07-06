69a61c28bbf5e3a7e096c58c4c570744bba362ed
Leslie <leslie.lianggangwei@foxmail.com>
2021-07-06 19:17:56 +0800

    整体优化

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -24,30 +24,22 @@ $h: 22px;
 $h2: $h - 4px;
 button {
   position: relative;
-  height: $h;
-  width: $h*2;
-  border: none;
-  background: grey;
+  height: $h; width: $h*2;
+  background: #bfbfbf;
   border-radius: $h/2;
+  border: none;
   outline: none;
-}
-
-button.switchStatus {
-  background: blue;
-}
-
-button.switchStatus span {
-  left: calc(100% - #{$h2} - 2px);
+  &.switchStatus {background: #1890ff;}
+  &.switchStatus span {left: calc(100% - #{$h2} - 2px);}
+  &:active span {width: $h2 + 4px;}
+  &.switchStatus:active span {width: $h2 + 4px; margin-left: -4px;}
 }
 
 span {
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
+  position: absolute;top: 2px;left: 2px;
+  height: $h2;width: $h2;
   background: white;
   border-radius: $h2 / 2;
-  transition: left 250ms;
+  transition: all 250ms;
 }
 </style>
--- src/view/Doc.vue
+++ src/view/Doc.vue
@@ -67,7 +67,7 @@ export default {
     main {
       flex-grow: 1;
       padding: 16px;
-      background: lightgreen;
+      background: white;
     }
   }
 }
