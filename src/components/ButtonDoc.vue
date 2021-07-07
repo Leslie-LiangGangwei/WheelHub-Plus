b01c07d5e0ef0bba628eb17c75b9a9942ef8430c
Leslie <leslie.lianggangwei@foxmail.com>
2021-07-07 11:50:45 +0800

    add Button

--- src/components/ButtonDoc.vue
+++ src/components/ButtonDoc.vue
@@ -1,10 +1,14 @@
 <template>
   <div>我是 Button 文档</div>
+  <Button>按钮</Button>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue'
+
 export default {
-  name: "ButtonDoc"
+  name: "ButtonDoc",
+  components: {Button}
 }
 </script>
 
---
+++ src/lib/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Button"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
