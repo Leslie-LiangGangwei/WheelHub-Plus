3989f5e4aaab452933e131cbd2ab557bb5d619e7
Leslie <leslie.lianggangwei@foxmail.com>
2021-07-08 18:16:08 +0800

    添加字体 14px

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -24,15 +24,16 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$font-size: 14px;
 .g-button {
+  display: inline-flex; justify-content: center; align-items: center;
   box-sizing: border-box;
   height: $h;
   padding: 0 12px;
   cursor: pointer;
-  display: inline-flex; justify-content: center; align-items: center;
   white-space: nowrap;
   background: white;
-  color: $color;
+  color: $color; font-size: $font-size;
   border: 1px solid $border-color; border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
   & + & {
