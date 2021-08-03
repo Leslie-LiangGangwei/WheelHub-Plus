dd6e561277194003c2eb68e1d5d6fb3e1ac47e40
Leslie <leslie.lianggangwei@foxmail.com>
2021-08-03 17:14:01 +0800

    完善 switchDoc 文档

--- src/components/SwitchDoc.vue
+++ src/components/SwitchDoc.vue
@@ -1,6 +1,23 @@
 <template>
-  <Switch v-model:value="switchValue"></Switch>
-  <p>{{switchValue}}</p>
+  <article class="markdown-body">
+    <h1>Switch 开关</h1>
+    <blockquote>
+      <p>开关选择器，可切换"开"和"关"两种状态</p>
+    </blockquote>
+    <div class="demo">
+      <p class="title">基础用法</p>
+      <p>最简单的用法。</p>
+      <div class="demo-container">
+        <div class="demo-component">
+          <Switch v-model:value="switchValue"/>
+        </div>
+        <div class="demo-code">
+          <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        </div>
+      </div>
+    </div>
+  </article>
+
 </template>
 
 <script lang="ts">
@@ -16,3 +33,30 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+$border-color: #efeff4;
+
+.demo {
+  .title {
+    font-size: 22px; font-weight: 400;
+    margin: 55px 0 20px;
+  }
+  .demo-container {
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+    .demo-component {
+      padding: 42px 24px 50px;
+    }
+    .demo-code {
+      padding: 8px 16px;
+      border-top: 1px dashed $border-color;
+      pre {
+        line-height: 1.1;
+        font-family: Consolas, 'Courier New', Courier, monospace;
+        margin: 0;
+      }
+    }
+  }
+}
+</style>
