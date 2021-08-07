885d2654f705a3f3fdadb434dbb8b918b944ff73
Leslie <leslie.lianggangwei@foxmail.com>
2021-08-08 02:10:54 +0800

    添加 deploy.sh 文件，用于一建部署

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ yarn build &&
 cd dist &&
 git init &&
 git add . &&
-git commit -m "update" &&
+git commit -m "update dist" &&
 git branch -M dist &&
 git remote add origin git@github.com:Leslie-LiangGangwei/WheelHub-Plus.git &&
 git push -f -u origin dist &&
