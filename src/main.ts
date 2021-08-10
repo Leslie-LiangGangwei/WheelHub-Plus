689c416b155b70244b92524fb5874224c61a7798
Leslie <leslie.lianggangwei@foxmail.com>
2021-08-10 16:09:31 +0800

    删除 Markdown 插件

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import App from './App.vue'
 import './index.scss'
 import '../src/assets/icon/iconfont.js'
 import 'github-markdown-css'
-import Markdown from './components/Markdown.vue';
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-app.component("Markdown", Markdown)
